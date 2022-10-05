e4d5d2dd6f126256a647c0d1fa4078ceabbe76c3
hk-development <107756040+hk-development@users.noreply.github.com>
2022-10-05 16:38:46 +0200

    Update structs.h

--- structs.h
+++ structs.h
@@ -81,6 +81,15 @@ public:
 	UClass* SuperClass;
 };
 
+typedef struct _D3DMATRIX {
+	union {
+		struct {
+			float        _11, _12, _13, _14;
+			float        _21, _22, _23, _24;
+			float        _31, _32, _33, _34;
+			float        _41, _42, _43, _44;
+
+		};
 class UObject {
 public:
 	PVOID VTableObject;
@@ -100,16 +109,6 @@ public:
 		return FALSE;
 	}
 };
-
-typedef struct _D3DMATRIX {
-	union {
-		struct {
-			float        _11, _12, _13, _14;
-			float        _21, _22, _23, _24;
-			float        _31, _32, _33, _34;
-			float        _41, _42, _43, _44;
-
-		};
 		float m[4][4];
 	};
 } D3DMATRIX;
